32e1e6085b2f4034e2fb74864e790271e23189e4
RUSHIKESH KHEDEKAR <rxshii09@gmail.com>
2023-08-28 19:50:17 +0530

    add new para

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import './App.css';
 function App() {
   return (
     <div className='bg-black text-white h-[100vh] flex justify-center items-center'>
-      <h1 className='text-[10vh] capitalize'>welcome to react app</h1>
+      <h1 className='text-[10vh] capitalize'>welcome to react app </h1>
+      <H1>HELLO THIS SIDE RUSHIKESH</H1>
     </div>
   );
 }
