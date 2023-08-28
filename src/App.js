326055b3ac9df3b3c222a416c61744262d2a5d54
preysent <preysent@gmail.com>
2023-08-28 19:26:00 +0530

    added new pera

--- src/App.js
+++ src/App.js
@@ -4,8 +4,11 @@ function App() {
   return (
     <div className='bg-black text-white h-[100vh] flex justify-center items-center'>
       <h1 className='text-[10vh] capitalize'>welcome to react app</h1>
+      <p> here is the change </p>
     </div>
   );
 }
+
+
 
 export default App;
